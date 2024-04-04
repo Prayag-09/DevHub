65d170e0f850ecace70231ca2b715d892b7b0b87
Prayag-09 <91512981+Prayag-09@users.noreply.github.com>
2024-04-04 22:27:30 +0530

    routes

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+const SignIn = () => {
+    return (
+        <div>
+            This is Sign in page !!
+        </div>
+    )
+}
+export default SignIn
---
+++ app/(root)/ask-question/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Question = () => {
+  return (
+    <div>
+      Ask your questions !!
+    </div>
+  )
+}
+
+export default Question
